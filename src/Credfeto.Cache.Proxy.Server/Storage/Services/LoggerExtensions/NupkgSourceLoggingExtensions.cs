3263422c665fa6c0126b38d300b110ed798e9445
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-05-15 18:36:36 +0000

    Cleanup: NupkgSourceLoggingExtensions.cs

--- src/Credfeto.Cache.Proxy.Server/Storage/Services/LoggerExtensions/NupkgSourceLoggingExtensions.cs
+++ src/Credfeto.Cache.Proxy.Server/Storage/Services/LoggerExtensions/NupkgSourceLoggingExtensions.cs
@@ -6,9 +6,21 @@ namespace Credfeto.Cache.Proxy.Server.Storage.Services.LoggerExtensions;
 
 internal static partial class NupkgSourceLoggingExtensions
 {
-    [LoggerMessage(LogLevel.Error, EventId = 1, Message = "Failed to retrieve NUPKG from {upstream} Received Http {statusCode}")]
-    public static partial void UpstreamPackageFailed(this ILogger<NupkgSource> logger, Uri upstream, HttpStatusCode statusCode);
+    [LoggerMessage(
+        LogLevel.Error,
+        EventId = 1,
+        Message = "Failed to retrieve NUPKG from {upstream} Received Http {statusCode}"
+    )]
+    public static partial void UpstreamPackageFailed(
+        this ILogger<NupkgSource> logger,
+        Uri upstream,
+        HttpStatusCode statusCode
+    );
 
-    [LoggerMessage(LogLevel.Information, EventId = 1, Message = "Retrieved Cached NUPKG from {upstream} Length: {length}")]
+    [LoggerMessage(
+        LogLevel.Information,
+        EventId = 1,
+        Message = "Retrieved Cached NUPKG from {upstream} Length: {length}"
+    )]
     public static partial void CachedPackage(this ILogger<NupkgSource> logger, Uri upstream, long length);
 }
