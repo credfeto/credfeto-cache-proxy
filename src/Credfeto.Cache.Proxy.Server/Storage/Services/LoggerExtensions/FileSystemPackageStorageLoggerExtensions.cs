c2015402231471e92ed31a4e3a9eb268759fe843
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-05-15 18:35:52 +0000

    Cleanup: FileSystemPackageStorageLoggerExtensions.cs

--- src/Credfeto.Cache.Proxy.Server/Storage/Services/LoggerExtensions/FileSystemPackageStorageLoggerExtensions.cs
+++ src/Credfeto.Cache.Proxy.Server/Storage/Services/LoggerExtensions/FileSystemPackageStorageLoggerExtensions.cs
@@ -6,8 +6,18 @@ namespace Credfeto.Cache.Proxy.Server.Storage.Services.LoggerExtensions;
 internal static partial class FileSystemPackageStorageLoggerExtensions
 {
     [LoggerMessage(LogLevel.Error, EventId = 1, Message = "Could not read cached package: {filename}: {message}")]
-    public static partial void FailedToReadFileFromCache(this ILogger<FileSystemPackageStorage> logger, string filename, string message, Exception exception);
+    public static partial void FailedToReadFileFromCache(
+        this ILogger<FileSystemPackageStorage> logger,
+        string filename,
+        string message,
+        Exception exception
+    );
 
     [LoggerMessage(LogLevel.Error, EventId = 3, Message = "Failed to save cached package: {filename}: {message}")]
-    public static partial void SaveFailed(this ILogger<FileSystemPackageStorage> logger, string filename, string message, Exception exception);
+    public static partial void SaveFailed(
+        this ILogger<FileSystemPackageStorage> logger,
+        string filename,
+        string message,
+        Exception exception
+    );
 }
