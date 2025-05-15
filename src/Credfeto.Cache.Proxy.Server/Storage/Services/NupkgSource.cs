d2a809df3fd94a3a23c63cee02d75fbc8903e86f
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-05-15 18:34:16 +0000

    Cleanup: NupkgSource.cs

--- src/Credfeto.Cache.Proxy.Server/Storage/Services/NupkgSource.cs
+++ src/Credfeto.Cache.Proxy.Server/Storage/Services/NupkgSource.cs
@@ -20,30 +20,59 @@ public sealed class NupkgSource : INupkgSource
         this._packageDownloader = packageDownloader;
     }
 
-    public async ValueTask<PackageResult?> GetFromUpstreamAsync(CacheServerConfig config, string path, ProductInfoHeaderValue? userAgent, CancellationToken cancellationToken)
+    public async ValueTask<PackageResult?> GetFromUpstreamAsync(
+        CacheServerConfig config,
+        string path,
+        ProductInfoHeaderValue? userAgent,
+        CancellationToken cancellationToken
+    )
     {
-        return await this.TryToGetFromCacheAsync(config: config, sourcePath: path, cancellationToken: cancellationToken) ??
-               await this.GetFromUpstream2Async(config: config, sourcePath: path, userAgent: userAgent, cancellationToken: cancellationToken);
+        return await this.TryToGetFromCacheAsync(config: config, sourcePath: path, cancellationToken: cancellationToken)
+            ?? await this.GetFromUpstream2Async(
+                config: config,
+                sourcePath: path,
+                userAgent: userAgent,
+                cancellationToken: cancellationToken
+            );
     }
 
-    private async ValueTask<PackageResult?> TryToGetFromCacheAsync(CacheServerConfig config, string sourcePath, CancellationToken cancellationToken)
+    private async ValueTask<PackageResult?> TryToGetFromCacheAsync(
+        CacheServerConfig config,
+        string sourcePath,
+        CancellationToken cancellationToken
+    )
     {
-        byte[]? data = await this._packageStorage.ReadFileAsync(Path.Combine(path1: config.Target, path2: sourcePath), cancellationToken: cancellationToken);
+        byte[]? data = await this._packageStorage.ReadFileAsync(
+            Path.Combine(path1: config.Target, path2: sourcePath),
+            cancellationToken: cancellationToken
+        );
 
-        return data is null
-            ? null
-            : new(Data: data, ShouldCache(config: config, sourcePath: sourcePath));
+        return data is null ? null : new(Data: data, ShouldCache(config: config, sourcePath: sourcePath));
     }
 
-    private async ValueTask<PackageResult?> GetFromUpstream2Async(CacheServerConfig config, string sourcePath, ProductInfoHeaderValue? userAgent, CancellationToken cancellationToken)
+    private async ValueTask<PackageResult?> GetFromUpstream2Async(
+        CacheServerConfig config,
+        string sourcePath,
+        ProductInfoHeaderValue? userAgent,
+        CancellationToken cancellationToken
+    )
     {
-        byte[] data = await this._packageDownloader.ReadUpstreamAsync(config: config, path: sourcePath, userAgent: userAgent, cancellationToken: cancellationToken);
+        byte[] data = await this._packageDownloader.ReadUpstreamAsync(
+            config: config,
+            path: sourcePath,
+            userAgent: userAgent,
+            cancellationToken: cancellationToken
+        );
 
         CacheSetting? cacheSetting = ShouldCache(config: config, sourcePath: sourcePath);
 
         if (cacheSetting is not null)
         {
-            await this._packageStorage.SaveFileAsync(sourcePath: sourcePath, buffer: data, cancellationToken: cancellationToken);
+            await this._packageStorage.SaveFileAsync(
+                sourcePath: sourcePath,
+                buffer: data,
+                cancellationToken: cancellationToken
+            );
         }
 
         return new PackageResult(Data: data, CacheSetting: cacheSetting);
@@ -56,9 +85,14 @@ public sealed class NupkgSource : INupkgSource
 
     private static bool IsMatchingPath(CacheSetting setting, string sourcePath)
     {
-        return Regex.IsMatch(input: sourcePath,
-                             pattern: setting.Match,
-                             RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.NonBacktracking | RegexOptions.Singleline,
-                             TimeSpan.FromSeconds(0.5));
+        return Regex.IsMatch(
+            input: sourcePath,
+            pattern: setting.Match,
+            RegexOptions.Compiled
+                | RegexOptions.ExplicitCapture
+                | RegexOptions.NonBacktracking
+                | RegexOptions.Singleline,
+            TimeSpan.FromSeconds(0.5)
+        );
     }
 }
