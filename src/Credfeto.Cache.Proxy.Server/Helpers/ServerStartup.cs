f1be79019a456add117728556b165c8e9b263ab4
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-05-15 18:25:03 +0000

    Cleanup: ServerStartup.cs

--- src/Credfeto.Cache.Proxy.Server/Helpers/ServerStartup.cs
+++ src/Credfeto.Cache.Proxy.Server/Helpers/ServerStartup.cs
@@ -58,11 +58,12 @@ internal static class ServerStartup
     {
         string configPath = GetServerConfig(out ServerConfig appConfig);
 
-        return WebApplication.CreateSlimBuilder(args)
-                             .ConfigureServices(appConfig: appConfig)
-                             .ConfigureAppHost()
-                             .ConfigureWebHost(configPath: configPath)
-                             .Build();
+        return WebApplication
+            .CreateSlimBuilder(args)
+            .ConfigureServices(appConfig: appConfig)
+            .ConfigureAppHost()
+            .ConfigureWebHost(configPath: configPath)
+            .Build();
     }
 
     private static string GetServerConfig(out ServerConfig appConfig)
@@ -79,31 +80,42 @@ internal static class ServerStartup
 
     private static WebApplicationBuilder ConfigureAppHost(this WebApplicationBuilder builder)
     {
-        builder.Host.UseWindowsService()
-               .UseSystemd();
+        builder.Host.UseWindowsService().UseSystemd();
 
         return builder;
     }
 
     private static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder, ServerConfig appConfig)
     {
-        builder.Services.AddSingleton(appConfig)
-               .AddDate()
-               .AddSingleton<CacheMiddleware>()
-               .AddSingleton<IPackageStorage, FileSystemPackageStorage>()
-               .AddSingleton<IPackageDownloader, PackageDownloader>()
-               .AddSingleton<INupkgSource, NupkgSource>()
-               .AddNupkgClient(appConfig)
-               .ConfigureHttpJsonOptions(options => options.SerializerOptions.TypeInfoResolverChain.Insert(index: 0, item: AppJsonContexts.Default));
+        builder
+            .Services.AddSingleton(appConfig)
+            .AddDate()
+            .AddSingleton<CacheMiddleware>()
+            .AddSingleton<IPackageStorage, FileSystemPackageStorage>()
+            .AddSingleton<IPackageDownloader, PackageDownloader>()
+            .AddSingleton<INupkgSource, NupkgSource>()
+            .AddNupkgClient(appConfig)
+            .ConfigureHttpJsonOptions(options =>
+                options.SerializerOptions.TypeInfoResolverChain.Insert(index: 0, item: AppJsonContexts.Default)
+            );
 
         return builder;
     }
 
     private static WebApplicationBuilder ConfigureWebHost(this WebApplicationBuilder builder, string configPath)
     {
-        builder.WebHost.UseKestrel(options: options => SetKestrelOptions(options: options, httpPort: HTTP_PORT, httpsPort: HTTPS_PORT, h2Port: H2_PORT, configurationFiledPath: configPath))
-               .UseSetting(key: WebHostDefaults.SuppressStatusMessagesKey, value: "True")
-               .ConfigureLogging((_, logger) => ConfigureLogging(logger));
+        builder
+            .WebHost.UseKestrel(options: options =>
+                SetKestrelOptions(
+                    options: options,
+                    httpPort: HTTP_PORT,
+                    httpsPort: HTTPS_PORT,
+                    h2Port: H2_PORT,
+                    configurationFiledPath: configPath
+                )
+            )
+            .UseSetting(key: WebHostDefaults.SuppressStatusMessagesKey, value: "True")
+            .ConfigureLogging((_, logger) => ConfigureLogging(logger));
 
         return builder;
     }
@@ -118,49 +130,62 @@ internal static class ServerStartup
     {
         configuration.Reload();
 
-        return new([
-                       new(Source: "builds.dotnet.local", Target: "https://builds.dotnet.microsoft.com", [new(Match: "\\.tar\\.gz$", LifeTimeSeconds: 63115200)])
-                   ],
-                   Storage: "/cache/proxy");
+        return new(
+            [
+                new(
+                    Source: "builds.dotnet.local",
+                    Target: "https://builds.dotnet.microsoft.com",
+                    [new(Match: "\\.tar\\.gz$", LifeTimeSeconds: 63115200)]
+                ),
+            ],
+            Storage: "/cache/proxy"
+        );
     }
 
-    [SuppressMessage(category: "Microsoft.Reliability", checkId: "CA2000:DisposeObjectsBeforeLosingScope", Justification = "Lives for program lifetime")]
-    [SuppressMessage(category: "SmartAnalyzers.CSharpExtensions.Annotations", checkId: "CSE007:DisposeObjectsBeforeLosingScope", Justification = "Lives for program lifetime")]
+    [SuppressMessage(
+        category: "Microsoft.Reliability",
+        checkId: "CA2000:DisposeObjectsBeforeLosingScope",
+        Justification = "Lives for program lifetime"
+    )]
+    [SuppressMessage(
+        category: "SmartAnalyzers.CSharpExtensions.Annotations",
+        checkId: "CSE007:DisposeObjectsBeforeLosingScope",
+        Justification = "Lives for program lifetime"
+    )]
     private static void ConfigureLogging(ILoggingBuilder logger)
     {
-        logger.ClearProviders()
-              .AddSerilog(CreateLogger(), dispose: true);
+        logger.ClearProviders().AddSerilog(CreateLogger(), dispose: true);
     }
 
     private static Logger CreateLogger()
     {
-        return new LoggerConfiguration().Enrich.WithDemystifiedStackTraces()
-                                        .Enrich.FromLogContext()
-                                        .Enrich.WithMachineName()
-                                        .Enrich.WithProcessId()
-                                        .Enrich.WithThreadId()
-                                        .Enrich.WithProperty(name: "ServerVersion", value: VersionInformation.Version)
-                                        .Enrich.WithProperty(name: "ProcessName", value: VersionInformation.Product)
-                                        .WriteToDebuggerAwareOutput()
-                                        .CreateLogger();
+        return new LoggerConfiguration()
+            .Enrich.WithDemystifiedStackTraces()
+            .Enrich.FromLogContext()
+            .Enrich.WithMachineName()
+            .Enrich.WithProcessId()
+            .Enrich.WithThreadId()
+            .Enrich.WithProperty(name: "ServerVersion", value: VersionInformation.Version)
+            .Enrich.WithProperty(name: "ProcessName", value: VersionInformation.Product)
+            .WriteToDebuggerAwareOutput()
+            .CreateLogger();
     }
 
     private static LoggerConfiguration WriteToDebuggerAwareOutput(this LoggerConfiguration configuration)
     {
         LoggerSinkConfiguration writeTo = configuration.WriteTo;
 
-        return Debugger.IsAttached
-            ? writeTo.Debug()
-            : writeTo.Console();
+        return Debugger.IsAttached ? writeTo.Debug() : writeTo.Console();
     }
 
     private static IConfigurationRoot LoadConfiguration(string configPath)
     {
-        return new ConfigurationBuilder().SetBasePath(configPath)
-                                         .AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: false)
-                                         .AddJsonFile(path: "appsettings-local.json", optional: true, reloadOnChange: false)
-                                         .AddEnvironmentVariables()
-                                         .Build();
+        return new ConfigurationBuilder()
+            .SetBasePath(configPath)
+            .AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: false)
+            .AddJsonFile(path: "appsettings-local.json", optional: true, reloadOnChange: false)
+            .AddEnvironmentVariables()
+            .Build();
     }
 
     private static void SetH1ListenOptions(ListenOptions listenOptions)
@@ -179,7 +204,13 @@ internal static class ServerStartup
         listenOptions.UseHttps(fileName: certFile);
     }
 
-    private static void SetKestrelOptions(KestrelServerOptions options, int httpPort, int httpsPort, int h2Port, string configurationFiledPath)
+    private static void SetKestrelOptions(
+        KestrelServerOptions options,
+        int httpPort,
+        int httpsPort,
+        int h2Port,
+        string configurationFiledPath
+    )
     {
         options.DisableStringReuse = false;
         options.AllowSynchronousIO = false;
@@ -193,7 +224,11 @@ internal static class ServerStartup
         if (httpsPort != 0 && File.Exists(certFile))
         {
             Console.WriteLine($"Listening on HTTPS port: {httpsPort}");
-            options.Listen(address: IPAddress.Any, port: httpsPort, configure: o => SetHttpsListenOptions(listenOptions: o, certFile: certFile));
+            options.Listen(
+                address: IPAddress.Any,
+                port: httpsPort,
+                configure: o => SetHttpsListenOptions(listenOptions: o, certFile: certFile)
+            );
         }
 
         if (h2Port != 0)
