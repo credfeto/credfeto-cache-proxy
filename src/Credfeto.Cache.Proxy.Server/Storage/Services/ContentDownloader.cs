ef76e6cee7dafbb609b3bb55745e9a6f63914671
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-09 11:41:32 +0100

    Added support for non-blocking HTTP requests by utilizing the `NonBlocking` library. Enhanced semaphore management to prevent simultaneous downloads of `.gz` files, improving performance and reliability in handling large datasets.

--- src/Credfeto.Cache.Proxy.Server/Storage/Services/ContentDownloader.cs
+++ src/Credfeto.Cache.Proxy.Server/Storage/Services/ContentDownloader.cs
@@ -10,11 +10,14 @@ using Credfeto.Cache.Proxy.Server.Extensions;
 using Credfeto.Cache.Proxy.Server.Storage.Services.LoggerExtensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using NonBlocking;
 
 namespace Credfeto.Cache.Proxy.Server.Storage.Services;
 
 public sealed class ContentDownloader : IContentDownloader
 {
+    private static readonly CancellationToken DoNotCancelEarly = CancellationToken.None;
+    private readonly ConcurrentDictionary<string, SemaphoreSlim> _connections = new(StringComparer.Ordinal);
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly ILogger<ContentDownloader> _logger;
 
@@ -24,39 +27,55 @@ public sealed class ContentDownloader : IContentDownloader
         this._logger = logger;
     }
 
-    public async ValueTask<byte[]> ReadUpstreamAsync(
-        CacheServerConfig config,
-        PathString path,
-        ProductInfoHeaderValue? userAgent,
-        CancellationToken cancellationToken
-    )
+    public async ValueTask<byte[]> ReadUpstreamAsync(CacheServerConfig config, PathString path, ProductInfoHeaderValue? userAgent, CancellationToken cancellationToken)
     {
         HttpClient client = this.GetClient(config: config, userAgent: userAgent, out Uri baseUri);
 
         Uri requestUri = MakeUri(baseUri: baseUri, path: path);
+        SemaphoreSlim? wait = await this.GetSemaphoreAsync(baseUri: requestUri, cancellationToken: cancellationToken);
 
-        using (
-            HttpResponseMessage result = await client.GetAsync(
-                requestUri: requestUri,
-                cancellationToken: cancellationToken
-            )
-        )
+        try
         {
-            if (result.IsSuccessStatusCode)
+            using (HttpResponseMessage result = await client.GetAsync(requestUri: requestUri, cancellationToken: DoNotCancelEarly))
             {
-                byte[] bytes = await result.Content.ReadAsByteArrayAsync(cancellationToken: cancellationToken);
+                if (result.IsSuccessStatusCode)
+                {
+                    byte[] bytes = await result.Content.ReadAsByteArrayAsync(cancellationToken: DoNotCancelEarly);
 
-                this._logger.UpstreamPackageOk(
-                    upstream: requestUri,
-                    statusCode: result.StatusCode,
-                    length: bytes.Length
-                );
+                    this._logger.UpstreamPackageOk(upstream: requestUri, statusCode: result.StatusCode, length: bytes.Length);
 
-                return bytes;
+                    return bytes;
+                }
+
+                return Failed(requestUri: requestUri, resultStatusCode: result.StatusCode);
             }
-
-            return Failed(requestUri: requestUri, resultStatusCode: result.StatusCode);
         }
+        finally
+        {
+            wait?.Release();
+        }
+    }
+
+    private async ValueTask<SemaphoreSlim?> GetSemaphoreAsync(Uri baseUri, CancellationToken cancellationToken)
+    {
+        if (!baseUri.PathAndQuery.EndsWith(".gz", StringComparison.OrdinalIgnoreCase))
+        {
+            return null;
+        }
+
+        string key = baseUri.DnsSafeHost;
+
+        if (this._connections.TryGetValue(key, out SemaphoreSlim? semaphore))
+        {
+            await semaphore.WaitAsync(cancellationToken);
+
+            return semaphore;
+        }
+
+        semaphore = this._connections.GetOrAdd(key, new SemaphoreSlim(1));
+        await semaphore.WaitAsync(cancellationToken);
+
+        return semaphore;
     }
 
     private static Uri MakeUri(Uri baseUri, PathString path)
@@ -69,17 +88,15 @@ public sealed class ContentDownloader : IContentDownloader
     [DoesNotReturn]
     private static byte[] Failed(Uri requestUri, HttpStatusCode resultStatusCode)
     {
-        throw new HttpRequestException(
-            $"Failed to download {requestUri}: {resultStatusCode.GetName()}",
-            inner: null,
-            statusCode: resultStatusCode
-        );
+        throw new HttpRequestException($"Failed to download {requestUri}: {resultStatusCode.GetName()}", inner: null, statusCode: resultStatusCode);
     }
 
     private HttpClient GetClient(CacheServerConfig config, ProductInfoHeaderValue? userAgent, out Uri baseUri)
     {
         baseUri = HttpClientNames.GetHttpClientUri(config: config, out string name);
 
-        return this._httpClientFactory.CreateClient(name).WithBaseAddress(baseUri).WithUserAgent(userAgent);
+        return this._httpClientFactory.CreateClient(name)
+                   .WithBaseAddress(baseUri)
+                   .WithUserAgent(userAgent);
     }
 }
--- src/Credfeto.Cache.Proxy.Server/Storage/Services/HttpClientSetup.cs
+++ src/Credfeto.Cache.Proxy.Server/Storage/Services/HttpClientSetup.cs
@@ -12,8 +12,8 @@ internal static class HttpClientSetup
 {
     private const int CONCURRENT_ACTIONS = 30;
     private const int QUEUED_ACTIONS = 10;
-    private static readonly TimeSpan HttpTimeout = TimeSpan.FromMinutes(5);
-    private static readonly TimeSpan HandlerTimeout = TimeSpan.FromMinutes(5);
+    private static readonly TimeSpan HttpTimeout = TimeSpan.FromMinutes(15);
+    private static readonly TimeSpan HandlerTimeout = TimeSpan.FromMinutes(15);
     private static readonly TimeSpan PollyTimeout = HttpTimeout.Add(TimeSpan.FromSeconds(1));
 
     public static IServiceCollection AddContentClient(this IServiceCollection services, ServerConfig appConfig)
@@ -22,20 +22,12 @@ internal static class HttpClientSetup
         {
             Uri target = HttpClientNames.GetHttpClientUri(config: config, out string name);
 
-            services = services
-                .AddHttpClient(
-                    name: name,
-                    configureClient: httpClient =>
-                        InitializeContentClient(upstreamUrl: target, httpClient: httpClient, httpTimeout: HttpTimeout)
-                )
-                .SetHandlerLifetime(HandlerTimeout)
-                .ConfigurePrimaryHttpMessageHandler(configureHandler: _ => new HttpClientHandler
-                {
-                    AutomaticDecompression = DecompressionMethods.All,
-                })
-                .AddPolicyHandler(Policy.BulkheadAsync<HttpResponseMessage>(CONCURRENT_ACTIONS * 2, QUEUED_ACTIONS * 2))
-                .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(PollyTimeout))
-                .Services;
+            services = services.AddHttpClient(name: name, configureClient: httpClient => InitializeContentClient(upstreamUrl: target, httpClient: httpClient, httpTimeout: HttpTimeout))
+                               .SetHandlerLifetime(HandlerTimeout)
+                               .ConfigurePrimaryHttpMessageHandler(configureHandler: _ => new HttpClientHandler { AutomaticDecompression = DecompressionMethods.All, })
+                               .AddPolicyHandler(Policy.BulkheadAsync<HttpResponseMessage>(CONCURRENT_ACTIONS * 2, QUEUED_ACTIONS * 2))
+                               .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(PollyTimeout))
+                               .Services;
         }
 
         return services;
@@ -47,9 +39,7 @@ internal static class HttpClientSetup
         httpClient.DefaultRequestVersion = HttpVersion.Version11;
         httpClient.BaseAddress = upstreamUrl;
         httpClient.DefaultRequestHeaders.Accept.Add(new(mediaType: "application/octet-stream"));
-        httpClient.DefaultRequestHeaders.UserAgent.Add(
-            new(new ProductHeaderValue(name: VersionInformation.Product, version: VersionInformation.Version))
-        );
+        httpClient.DefaultRequestHeaders.UserAgent.Add(new(new ProductHeaderValue(name: VersionInformation.Product, version: VersionInformation.Version)));
         httpClient.Timeout = httpTimeout;
     }
 }
