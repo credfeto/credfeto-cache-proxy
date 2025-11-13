7627e43c0a27c045484c006d5fa6dc23fa8001db
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-11-13 01:20:47 +0000

    Cleanup: IPackageStorage.cs

--- src/Credfeto.Cache.Proxy.Storage.Interfaces/IPackageStorage.cs
+++ src/Credfeto.Cache.Proxy.Storage.Interfaces/IPackageStorage.cs
@@ -10,6 +10,6 @@ public interface IPackageStorage
 {
     Stream? ReadFile(string sourceHost, string sourcePath);
 
-    [SuppressMessage(category: "Meziantou.Analyzer", checkId: "MA0109: Add an overload with a Span or Memory parameter", Justification = "Won't work here")]
+    
     ValueTask SaveFileAsync(string sourceHost, string sourcePath, Func<Stream, CancellationToken, Task> readAsync, CancellationToken cancellationToken);
 }
