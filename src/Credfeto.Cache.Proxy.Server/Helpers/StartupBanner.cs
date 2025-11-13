89fface0c882d6b48925d4fd187b93e3cc91e493
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-11-13 22:51:37 +0000

    Source generated StartupBanner for process

--- src/Credfeto.Cache.Proxy.Server/Helpers/StartupBanner.cs
+++ src/Credfeto.Cache.Proxy.Server/Helpers/StartupBanner.cs
@@ -1,22 +1,15 @@
 using System;
+using Figgle;
 
 namespace Credfeto.Cache.Proxy.Server.Helpers;
 
-internal static class StartupBanner
+// https://www.figlet.org/examples.html
+[GenerateFiggleText("Banner", "basic", "Cache Proxy")]
+internal static partial class StartupBanner
 {
     public static void Show()
     {
-        // Generated from https://fsymbols.com/generators/carty/
-        const string banner =
-            @"
-░█████╗░░█████╗░░█████╗░██╗░░██╗███████╗  ██████╗░██████╗░░█████╗░██╗░░██╗██╗░░░██╗
-██╔══██╗██╔══██╗██╔══██╗██║░░██║██╔════╝  ██╔══██╗██╔══██╗██╔══██╗╚██╗██╔╝╚██╗░██╔╝
-██║░░╚═╝███████║██║░░╚═╝███████║█████╗░░  ██████╔╝██████╔╝██║░░██║░╚███╔╝░░╚████╔╝░
-██║░░██╗██╔══██║██║░░██╗██╔══██║██╔══╝░░  ██╔═══╝░██╔══██╗██║░░██║░██╔██╗░░░╚██╔╝░░
-╚█████╔╝██║░░██║╚█████╔╝██║░░██║███████╗  ██║░░░░░██║░░██║╚█████╔╝██╔╝╚██╗░░░██║░░░
-░╚════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝  ╚═╝░░░░░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░";
-
-        Console.WriteLine(banner);
+        Console.WriteLine(Banner);
 
         Console.WriteLine("Starting version " + VersionInformation.Version + "...");
     }
